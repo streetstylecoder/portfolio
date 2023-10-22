1cd9b4a0d3790f5c9e0e7bbd7203f03060054aca
streetstylecoder <iamlivewire123@gmail.com>
2023-10-22 12:29:25 +0530

    personalized  changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Harsh Dhariwal",
+  title: "Backend and Machine learning developer",
+  email: "harshdhariq=wal29@gmail.com",
+  gitHub: "streetstylecoder",
+  instagram: "harshdhariwal__",
+  linkedIn: "harsh_dhariwal",
   medium: "",
-  twitter: "microsoft",
+  twitter: "harshdhariwal_",
   youTube: "Code",
 };
 
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,23 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "KISAN MITRA",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Kisan mitra is a ML powered application which enables farmers to know more about their farms guiding them what they should really grow depending on thier location and the soil at that place",
+    url: "https://github.com/streetstylecoder/Kisan_Mitra",
   },
   {
-    title: "Web Development for Beginners",
+    title:"GREENOVATE",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "ML based carbon accouting assistant for companies to attain their environment goals",
+    url: "https://github.com/streetstylecoder/greenovate",
   },
   {
-    title: "My Resume Site",
+    title: "EVORA",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+      "An android application which helps you decide whether you should switch to a electric car and also suggests you , cars you can opt for according to your needs",
+    url: "https://github.com/streetstylecoder/evora",
+  }
 ];
 
 const Portfolio = () => {
